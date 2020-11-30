b43860b96ef11d86eea944c2e85b1ee3b8af98c2
Christopher <cg.radiant@gmail.com>
2020-11-30 19:29:55 +0800

    Add light mode and dark mode functionality

---
+++ resources/script.js
@@ -0,0 +1,9 @@
+let lightMode = () => {
+    document.querySelector("main").style.backgroundImage = "url('resources/images/background-img-light.jpg')";
+    document.querySelector("main").style.color = "black";
+}
+
+let darkMode = () => {
+    document.querySelector("main").style.backgroundImage = "url('resources/images/background-img-dark.jpg')";
+    document.querySelector("main").style.color = "white";
+}
